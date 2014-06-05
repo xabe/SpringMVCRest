45a4684087dd50bb9db8677ce7d6eec4297130ad
unknown <47518418G@VDI-JBOSS-W7-05.inspeccion.dom>
2014-06-05 11:59:25 +0200

    Error

--- sql/data.sql
+++
@@ -1,6 +0,0 @@
-insert into t_profesor (id,nombre) values (1,'Jose');
-insert into t_profesor (id,nombre) values (2,'Ana');
-
-
-insert into t_curso (titulo,nivel,horas,activo,profesor,documento) values ('JAVA','basico',10,true,1,'');
-insert into t_curso (titulo,nivel,horas,activo,profesor,documento) values ('JAVA','basico',11,true,2,'');
