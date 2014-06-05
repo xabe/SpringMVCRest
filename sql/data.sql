552616009e8a3c15e2a19b438b6d2f6aa182825e
unknown <47518418G@VDI-JBOSS-W7-05.inspeccion.dom>
2014-06-05 11:56:34 +0200

    Mejoras en log y Extensiones en la URL

---
+++ sql/data.sql
@@ -0,0 +1,6 @@
+insert into t_profesor (id,nombre) values (1,'Jose');
+insert into t_profesor (id,nombre) values (2,'Ana');
+
+
+insert into t_curso (titulo,nivel,horas,activo,profesor,documento) values ('JAVA','basico',10,true,1,'');
+insert into t_curso (titulo,nivel,horas,activo,profesor,documento) values ('JAVA','basico',11,true,2,'');
